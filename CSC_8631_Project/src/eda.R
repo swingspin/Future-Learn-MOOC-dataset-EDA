b196dfe1b97f8c6df273827edf47474d82b3c6ac
swingspin <parthshah77777.ps@gmail.com>
2021-11-30 21:50:46 +0000

    EDA Q3 cycle,saving graphs in sub-directory

--- CSC_8631_Project/src/eda.R
+++ CSC_8631_Project/src/eda.R
@@ -13,11 +13,13 @@ load.project()
 #01-Finding the average drop in viewership from sept,2017 to sept,2018
 
 #Graphical representation of total views based on courses for 2 different months with a gap of a year
-plot_j1 = ggplot(Video_stats, aes(step_position, total_views, fill = run_month_year))+geom_col(position = 'dodge') + labs(title = "Analysis of Total views of the courses between two months")+
-  ggtitle("Analysis of Total views of the courses between months-A year apart")
-#Save the plot1 in the graphs directory
+plot_j1 = ggplot(Video_stats, aes(step_position, total_views, fill = run_month_year, size=100))+
+ geom_col(position = 'dodge') +
+  ggtitle("Analysis of Total views of the courses between months that are a year apart")
+#Save the plot_j1 in the graphs directory
 plot_j1
-#ggsave(file.path('graphs','plot_j1.pdf'))
+ggsave(file.path('graphs','plot_j1.pdf'))
+
 
 #creating a data frame named drop_percentage
 drop_percentage = as.data.frame(matrix(nrow = 0, ncol = 2));
@@ -48,17 +50,18 @@ average_drop = (sum(drop_percentage[,2])/13)
 
 #plotting the percentage_drop with respect to the step id of each part of the video lecture
 dropplot_1 = barplot(drop_percentage$percentage ~ drop_percentage$step_position, xlab = "step_position", ylab = "percentage")
-
+#Save the dropplot_1 in the graphs directory
+ggsave(file.path('graphs','dropplot_1.pdf'))
 #cycle 02
 #checking weather the yearly_drop from september,2017 to september,2018 has any correlation with mid_year_drop from september,2017 to february,2018
 #01-cycle 2 -Finding the average drop in viewership from sept,2017 to feb,2018
 
 #Graphical representation of total views based on courses for 2 different months with a gap of a year
-plot_j2 = ggplot(Video_stats_Q1_Cycle02, aes(step_position, total_views, fill = run_month_year))+geom_col(position = 'dodge') + labs(title = "Analysis of Total views of the courses between two months")+
+plot_j2 = ggplot(Video_stats_Q1_Cycle02, aes(step_position, total_views, fill = run_month_year))+geom_col(position = 'dodge') + 
   ggtitle("Analysis of Total views of the courses between months- approx half year apart")
-#Save the plot1 in the graphs directory
+#Save the plot_j2 in the graphs directory
 plot_j2
-#ggsave(file.path('graphs','plot_j2.pdf'))
+ggsave(file.path('graphs','plot_j2.pdf'))
 
 #creating a data frame named drop_percentage_mid_yearly
 drop_percentage_mid_yearly = as.data.frame(matrix(nrow = 0, ncol = 2));
@@ -84,7 +87,8 @@ average_drope_mid_yearly = (sum(drop_percentage_mid_yearly[,2])/13)
 
 #plotting the percentage_drop with respect to the step id of each part of the video lecture
 dropplot_2 = barplot(drop_percentage_mid_yearly$percentage ~ drop_percentage_mid_yearly$step_position, xlab = "step_position", ylab = "percentage")
-
+#Save the dropplot_2 in the graphs directory
+ggsave(file.path('graphs','dropplot_2.pdf'))
 
 
 #USE OF CORRELATION TO EXPLORE THE DATA and TO FIND CONCLUSIONS-------------------------------
@@ -100,7 +104,7 @@ cor(cyber.security.5_video.stats[,3], cyber.security.5_video.stats[,9:15])
 cor(cyber.security.7_video.stats[,3], cyber.security.7_video.stats[,9:15])
 #checking weather is there any major change in correlation data in the time difference of 1 year apart runned data collectively
 cor(Video_stats_Q2_Cycle01[,3], Video_stats_Q2_Cycle01[,9:15])
-or
+#OR
 #cor(Video_stats[,3], Video_stats[,9:15])
 
 #checking whether is there any major change in correlation data in the time difference of 5months i.e. approx half year apart runned data collectively
@@ -133,7 +137,7 @@ plot_A = ggplot(Video_stats, aes(total_downloads, total_transcript_views, color 
   ggtitle("Analysis of total downloads made as transcripts")
 plot_A
 #Save the plot_A in the graphs directory
-#ggsave(file.path('graphs','plot_A.pdf'))
+ggsave(file.path('graphs','plot_A.pdf'))
 
 #Using Correlation for Analysis of total downloads made as transcripts,in the time difference of half year apart runned data collectively
 cor(Video_stats_Q1_Cycle02[,5], Video_stats_Q1_Cycle02[,7])
@@ -142,8 +146,7 @@ plot_B = ggplot(Video_stats_Q1_Cycle02, aes(total_downloads, total_transcript_vi
   ggtitle("Analysis of total downloads made as transcripts")
 plot_B
 #Save the plot_B in the graphs directory
-#ggsave(file.path('graphs','plot_B.pdf'))
-
+ggsave(file.path('graphs','plot_B.pdf'))
 
 #EDA using graphical tool for analysis and TO FIND some conclusions------------------------------
 
@@ -155,45 +158,51 @@ plot_G1 = ggplot(Video_stats, aes(step_position, europe_views_percentage, color 
   ggtitle("Analysis of the courses based on views percentage from Europe region",)
 plot_G1
 #Save the plot_G1 in the graphs directory
-#ggsave(file.path('graphs','plot_G1.pdf'))
+ggsave(file.path('graphs','plot_G1.pdf'))
 
 #Plot_G1 Europe view percentage for each courses on different months
 plot_G2 = ggplot(Video_stats, aes(step_position, asia_views_percentage, color = run_month_year, size = 50)) + geom_point() +
   ggtitle("Analysis of the courses based on views percentage from Asia region",)
 plot_G2
-
+#Save the plot_G2 in the graphs directory
+ggsave(file.path('graphs','plot_G2.pdf'))
 
 #Plot_G1 Europe view percentage for each courses on different months
 plot_G3 = ggplot(Video_stats, aes(step_position, north_america_views_percentage, color = run_month_year, size = 50)) + geom_point() +
   ggtitle("Analysis of the courses based on views percentage from North America region",)
 plot_G3
+#Save the plot_G3 in the graphs directory
+ggsave(file.path('graphs','plot_G3.pdf'))
 
 #Plot_G1 Europe view percentage for each courses on different months
 plot_G4 = ggplot(Video_stats, aes(step_position, africa_views_percentage, color = run_month_year, size = 50)) + geom_point() +
   ggtitle("Analysis of the courses based on views percentage from African region",)
 plot_G4
+#Save the plot_G4 in the graphs directory
+ggsave(file.path('graphs','plot_G4.pdf'))
 
 #cycle_02
 #Analysis of the courses based on views percentage from major devices used using graphical tool for summary-taking data_files of one year apart for exploration
 
 #Plot dv 1
-plot_dv1 = ggplot(video_stats, aes(total_views, desktop_device_percentage, color = month, size = total_views)) + geom_point() +
-  ggtitle(wrapper("Analysis of desktop users based on total views of the modules", width = 50))
-#Save the plot5 in the graphs directory
-ggsave(file.path('graphs','plot5.pdf'))
+plot_dv1 = ggplot(Video_stats, aes(total_views, desktop_device_percentage, color = run_month_year, size = total_views)) + geom_point() +
+  ggtitle("Analysis of desktop users based on total views of the modules")
+plot_dv1
+#Save the plot_dv1 in the graphs directory
+ggsave(file.path('graphs','plot_dv1.pdf'))
 
 #Plot dv 2
-plot_dv2 = ggplot(video_stats, aes(total_views, mobile_device_percentage, color = month, size = total_views)) + geom_point() +
-  ggtitle(wrapper("Analysis of mobile users based on total views of the modules", width = 50))
-#Save the plot5 in the graphs directory
-ggsave(file.path('graphs','plot5.pdf'))
+plot_dv2 = ggplot(Video_stats, aes(total_views, mobile_device_percentage, color = run_month_year, size = total_views)) + geom_point() +
+  ggtitle("Analysis of mobile users based on total views of the modules")
+#Save the plot_dv2 in the graphs directory
+ggsave(file.path('graphs','plot_dv2.pdf'))
 
 
 #Plot dv 3
-plot_dv3= ggplot(video_stats, aes(total_views, tablet_device_percentage, color = month, size = total_views)) + geom_point() +
-  ggtitle(wrapper("Analysis of tablet users based on total views of the modules", width = 50))
-#Save the plot5 in the graphs directory
-ggsave(file.path('graphs','plot5.pdf'))
+plot_dv3= ggplot(Video_stats, aes(total_views, tablet_device_percentage, color = run_month_year, size = total_views)) + geom_point() +
+  ggtitle("Analysis of tablet users based on total views of the modules")
+#Save the plot_dv3 in the graphs directory
+ggsave(file.path('graphs','plot_dv3.pdf'))
 
 
 #some more graphical analysis,to explore and understand data more by business perspective
@@ -202,14 +211,15 @@ plot_x = ggplot(Video_stats) + geom_col(aes( run_month_year, total_downloads, fi
   ggtitle("Analysis of modules downloaded in different months")
 plot_x
 #Save the plot_x in the graphs directory
-#ggsave(file.path('graphs','plot_x.pdf'))
+ggsave(file.path('graphs','plot_x.pdf'))
 
 #plot_y
 plot_y = ggplot(Video_stats_Q1_Cycle02) + geom_col(aes( run_month_year, total_downloads, fill=factor(step_position)), position="dodge") + labs(fill = "Course modules") +
   ggtitle("Analysis of modules downloaded in different months")
 plot_y
-#Save the plot7 in the graphs directory
+#Save the plot_y in the graphs directory
+ggsave(file.path('graphs','plot_y.pdf'))
 
 
-
+#----------------x-----------------------------x------------------------x------------------------------x---------------------------------
 
